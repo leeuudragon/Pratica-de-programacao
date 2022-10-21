92e95e38312381c47632cce5db21d0e730c193ac
Leeuudragon <leeuu_sousa@hotmail.com>
2022-10-21 14:35:42 -0300

    upload do programa circunferencia do circulo

---
+++ Circunferencia.java
@@ -0,0 +1,9 @@
+public class Circunferencia {
+    static double circunferencia(double raio){
+        return 2 * Math.PI * raio;
+    }
+
+    public static void main(String[] args) {
+        System.out.print(circunferencia(10));
+    }
+}
